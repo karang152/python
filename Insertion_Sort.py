69fb6c61e477491958e73b153c7fc13e35d77ae4
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-05-10 11:57:40 +0530

    Create Insertion_Sort.py

---
+++ Insertion_Sort.py
@@ -0,0 +1,14 @@
+# Insertion Sort
+arr = [5, 2, 8, 1, 6]
+print("Original array:", arr)
+
+n = len(arr)
+for i in range(1, n):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+    arr[j + 1] = key
+
+print("Sorted array:", arr)
